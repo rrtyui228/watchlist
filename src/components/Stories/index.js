f5405756f2e4c3b19ad96f8ef4b7abd84bf1ed6b
rrtyui228 <solonin1998@gmail.com>
2022-03-12 17:49:21 +0500

    added grid to page

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {inject, observer} from 'mobx-react';
 import PropTypes from 'prop-types';
 import Story from '../Story';
+import s from './Stories.module.scss';
 
 @inject(({WatchlistStore}) => {
   return {
@@ -13,9 +14,15 @@ class Stories extends React.Component {
   render() {
     const {stories} = this.props;
 
-    return stories.map((story) => (
-      <Story key={story.id} story={story} />
-    ));
+    return (
+      <div className={s.stories}>
+        {
+          stories.map((story) => (
+            <Story key={story.id} story={story} />
+          ))
+        }
+      </div>
+    );
   }
 }
 
